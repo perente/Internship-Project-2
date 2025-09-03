114672be9b60df4bb1505aebbc74317fda60a430
AlicanCopur <alicancpr.33@gmail.com>
2025-09-03 09:48:07 +0300

    create head partial

--- web/src/router/dashboard.ts
+++ web/src/router/dashboard.ts
@@ -4,6 +4,7 @@ import path from "path";
 export function createDashboardServer() {
     const app = express();
 
+    app.use(express.static('public'));
     app.set("views", path.join(__dirname, "../views"));
     app.set("view engine", "ejs");
 
